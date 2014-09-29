d0d22b2f30e2f26d55f80720e1d10b0b81406030
Vikram Mishra <Vikram.Mishra@symphonyteleca.com>
2014-09-29 15:00:42 +0530

    initial commit for my blog

---
+++ full_stack_ror/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FullStackRor::Application.config.secret_token = '18ef5571f44ca1080dd6070c32c8d477289bf30f6fc609d65db27721c1d813cf410cced9062f435506298cc8fa7a586a436439c17dd9341d39e03293e404e9a2'
